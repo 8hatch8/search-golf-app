7dea009a122f852817887facbd1f33e1bfe0df1b
8hatch8 <yutaka.pg@gmail.com>
2022-03-22 14:12:26 +0900

    add: axiosを導入

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,15 +5,34 @@ import DatePicker, { registerLocale } from "react-datepicker";
 import ja from "date-fns/locale/ja";
 import "react-datepicker/dist/react-datepicker.css";
 
+import axios from "axios";
+
 const Home = () => {
   const Today = new Date();
   const [date, setDate] = React.useState(Today);
   registerLocale("ja", ja);
 
+  // formタグでbuttonをクリックするとデフォルトでonSubmitイベントが走る
+  // event.preventDefaultでonSubmitイベントをキャンセルしている
+  const onFormSubmit = async (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    const url =
+      "https://l1kwik11ne.execute-api.ap-northeast-1.amazonaws.com/production/golf-courses";
+    const response = await axios.get(url, {
+      params: {
+        date: "2022/2/22",
+        budget: "8000",
+        departure: "東京駅",
+        duration: "60分",
+      },
+    });
+    console.log(response);
+  };
+
   return (
     <div className='ui container' id='container'>
       <div className='Search__Form'>
-        <form className='ui form segment'>
+        <form className='ui form segment' onSubmit={onFormSubmit}>
           <div className='field'>
             <label>
               <i className='calendar alternate outline icon'></i>プレー日
